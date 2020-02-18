d577dc47d37f241834b2f3dbaa29fd2c4a8b4653
Kayzredman <Kayzredman@gmail.com>
2020-02-18 15:58:04 +0000

    After addn suer and message table to schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "rowland", password: "rowland1")
+User.create(username: "anaelle", password: "anaelle1")
+User.create(username: "ethan", password: "ethan1")
