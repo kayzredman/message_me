2a2393b4a4eaf22711e2a87e247d3b5e13f75aae
Kayzredman <Kayzredman@gmail.com>
2020-03-12 20:52:59 +0000

    Add msg from UI

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -6,6 +6,7 @@ class ChatroomController < ApplicationController
     end
 
     def index
+        @message = Message.new                                                                                                                                                        
          @messages = Message.all
     end
 
